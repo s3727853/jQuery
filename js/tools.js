1e5f478eac4bf4edb3a1d6f8d37b65101b3fdc47
Jack Edwards <s3727853@student.rmit.edu.au>
2020-02-17 17:26:30 +1100

    final changes

--- js/tools.js
+++ js/tools.js
@@ -1,50 +1,128 @@
 $(document).ready(function () {
- 
-   
+
+
 // ========= Page Transitions =========
     
 // Handles page transitions using jQueryUI slide effects  
     
 $("#foodLink").click(function(){
-           
                     $("#homepage").toggle("slide", {direction: "left"}, 600 );
-    
                     $("#foodcreator").toggle("slide", {direction: "right"}, 600);
                  });
     
 $("#nutritionLink").click(function(){
-           
                     $("#homepage").toggle("slide", {direction: "left"}, 600 );
-    
                     $("#nutrition").toggle("slide", {direction: "right"}, 600);
                  });
 
 //Clicking the company name at the top of each page will take user back to home page    
-$("#bth").click(function(){
-    
+$(".bth").click(function(){
 //                  Set a variable with the current visible page so we can transition it out
                     var currentPage = $(".page:visible");
-    
-                  
-//                  Only transition to homepage of we are not on homepage
+//                  Only transition to homepage if we are not on homepage
                     if ($("#homepage").is(":hidden")){
-
                         $("#homepage").toggle("slide", {direction: "left"}, 600 );
-                        
                         currentPage.toggle("slide", {direction: "right"}, 600);
                     } 
-    
                  });
     
+// ========= Menu =========
+    
+// Check if selected page is already visible, if not close menu and transition to new page.
+// if we are already on the page close the menu.
+    
+// Please note the menu is currently code heavy and needs to be optimized. Specific functions to handle page transitions would be ideal that take current page & next page as input variables.
+    
+    $(".navAbout").click(function(){
+        // Check if page is hidden (in other words we are not already on that page)
+        if ($("#about").is(":hidden")){
+            // Get the id of the current visible page so we can slide it out
+            var currentPage = $(".page:visible");
+            // Close the menu
+            $("#myNav").removeClass("active");
+            // Transition in page
+            $("#about").toggle("slide", {direction: "down"}, 600 );
+            // Transition out previous page
+            currentPage.toggle("slide", {direction: "up"}, 600);
+            // Change menu icon back to default state
+            $(".hamburger").attr("src", "images/menu.png");
+        } else {
+            // If we are already currently on the page, close menu and reset hamburger image
+            $("#myNav").removeClass("active");
+            $(".hamburger").attr("src", "images/menu.png");
+        }
+    });
+    
+    $(".navContact").click(function(){
+        if ($("#contact").is(":hidden")){    
+            var currentPage = $(".page:visible");
+            $("#myNav").removeClass("active");$("#myNav").removeClass("active");
+            $("#contact").toggle("slide", {direction: "down"}, 600 );
+            currentPage.toggle("slide", {direction: "up"}, 600);
+            $(".hamburger").attr("src", "images/menu.png");
+        } else {
+            $("#myNav").removeClass("active");
+            $(".hamburger").attr("src", "images/menu.png");
+        }  
+    });
+    
+    $(".navNutrition").click(function(){
+        if ($("#nutrition").is(":hidden")){
+            var currentPage = $(".page:visible");
+            $("#myNav").removeClass("active");
+            $("#nutrition").toggle("slide", {direction: "down"}, 600 );
+            currentPage.toggle("slide", {direction: "up"}, 600);
+            $(".hamburger").attr("src", "images/menu.png");
+        } else {
+            $("#myNav").removeClass("active");
+            $(".hamburger").attr("src", "images/menu.png");
+        }   
+    });
+    
+    $(".navHome").click(function(){
+        if ($("#homepage").is(":hidden")){
+            var currentPage = $(".page:visible");
+            $("#myNav").removeClass("active");
+            $("#homepage").toggle("slide", {direction: "down"}, 600 );
+            currentPage.toggle("slide", {direction: "up"}, 600);
+            $(".hamburger").attr("src", "images/menu.png");
+        } else {
+            $("#myNav").removeClass("active");
+            $(".hamburger").attr("src", "images/menu.png");
+        }
+    });
+    
+    // Open/Close mechanics of the menu and manage hamburger image
+    
+    $(".hamburger").click(function() { 
+        // Open the menu
+        $("#myNav").toggleClass("active");
+        
+        if($("#myNav").hasClass("active")){
+            // Change the menu icon to an X
+            $(".hamburger").attr("src", "images/close.png");
+            
+            // Draw svgs in menu by calling plugin function
+            var svg = $("svg").drawsvg({duration: 1300});
+            
+            // Santos, Leonardo. “jQuery Drawsvg plugin.”
+	        //     http://leocs.me/jquery-drawsvg/
+            
+            
+            svg.drawsvg("animate");
+            }
+    
+        else {
+            $(".hamburger").attr("src", "images/menu.png");
+        }
+    });
 
 
     
     
     // ========= Food Creator =========
     
-
-    var currentFieldset, nextFieldset, previousFieldset; // Fieldsets
-    var transitioning // use this to check that an animation isn't already underway
+    var currentFieldset, nextFieldset, previousFieldset; // Fieldsets    
     
     $(".next").click(function() {
         
@@ -61,38 +139,22 @@ $("#bth").click(function(){
                 breed: {
                     required: true,
                 },
-             
-               
-    			
+             	
     		},
     		messages: {
     			dogName: "Please enter your dogs name",
-                breed: "Please enter your dogs breed",
-				
+                breed: "Please enter your dogs breed",	
 			}
 	    });
         
         if ((!$('#multistep').valid())) {
        		return false;
     	}
-        
-        
-        
-        console.log("Next clicked");
-                     
-//        if(transitioning) return false;
-//        transitioning = true;
-    
-  
+            
         // Set the current and next fieldset variables
         currentFieldset = $(this).parent();
-        
-        
         nextFieldset = $(this).parent().next();
         
-        console.log("Next FS ");
-        console.log(nextFieldset);
-    
         //add next step on progress bar by adding class="active" to the html
         // at the specified index we set above (nextFieldset)
     
@@ -102,11 +164,7 @@ $("#bth").click(function(){
         nextFieldset.toggle("slide", {direction: "right"}, 700 );
         currentFieldset.toggle("slide", {direction: "left"}, 700 );
    
-    
     });
-    
-    
-    
     
     // ========= Food Creator Form Elements and Logic =========
     
@@ -129,13 +187,9 @@ $("#bth").click(function(){
     // ========= Populate Dog Age dropdowns =========
     
     // This may be overkill but keeps the HTML cleaner without hardcoding dropdown options.
-    
-    
     $(function () {
-
         var years = $("#years");
         var months =$("#months");
-
         //Loop to add year values to dropdown.
         for (var i = 1; i <= 15; i++) {
             var option = $("<option />");
@@ -144,7 +198,7 @@ $("#bth").click(function(){
             years.append(option);
         }
          //Loop to add year month to dropdown.
-        for (var i = 1; i <= 12; i++) {
+        for (var i = 1; i <= 11; i++) {
             var option = $("<option />");
             option.html(i);
             option.val(i);
@@ -153,15 +207,13 @@ $("#bth").click(function(){
     });
     
 // Use dog name in form Step 2 - Pet Details
-    
     // Input element
     $("#petName").on("change", function() {
         // Element we wanto append value to (paragraph)
         $(".appendName").append(this.value);
-   
-});
+    });
     
-  // On checkbox state change get the value of any other checkbox elements that are not the selected one and set checked to false.
+  // On checkbox state change get the value of any other checkbox elements that are not the active one, and set checked to false.
     $('input[type="checkbox"]').on('change', function() {
         $('input[type="checkbox"]').not(this).prop('checked', false);
     });
@@ -169,34 +221,33 @@ $("#bth").click(function(){
     
 //========= Final product from food creator form =========    
 
-$(".finish").click(function() {
+    $(".finish").click(function() {
     
-    // Transition food creator section off screen
-    $("#foodcreator").toggle("slide", {direction: "left"}, 600);
-    // Transition final page onto screen
-    $("#finalProduct").toggle("slide", {direction: "right"}, 600 );
-    
-    $(".fa").addClass("animate");
-    // Draw svg
-    var svg = $("svg").drawsvg({duration: 3500});
-    svg.drawsvg("animate");
-});
+        // Transition food creator section off screen
+        $("#foodcreator").toggle("slide", {direction: "left"}, 600);
+        // Transition final page onto screen
+        $("#finalProduct").toggle("slide", {direction: "right"}, 600 );
+        // Reset form
+        $("#multistep").trigger("reset");
+        // Note: while the form resets, more work needs to be done to actually reset
+        // input windows and start from the start the beginning of the food creator. outside of my scope for this project.
+    });
 
-    // Accodrian containing product info
-    // adapted from Vikas Verma, jQuery Accordian
-    //       https://codepen.io/vikasverma93/pen/raxGaM/ 
+        // Accodrian containing product info
+        // adapted from Vikas Verma, jQuery Accordian
+        //       https://codepen.io/vikasverma93/pen/raxGaM/ 
     
-    // Adapted to use local + and - images rather than fontawesome
+        // Adapted to use local + and - images rather than fontawesome
     
-$(".set > a").on("click", function() {
+    $(".set > a").on("click", function() {
          
-    if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        $(this)
-        .siblings(".content")
-         .slideUp(200);
-        $(".set > a img")
-        .attr("src", "images/plus.png");
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this)
+            .siblings(".content")
+                .slideUp(200);
+            $(".set > a img")
+                .attr("src", "images/plus.png");
         
         } else {
          $(".set > a img")
@@ -212,29 +263,95 @@ $(".set > a").on("click", function() {
             $(this)
                 .siblings(".content")
                 .slideDown(200);
-            }         
-  });
-    
-// ======= Menu ======
-    
+        }         
+    });
 
-// simple if/else to open close
     
-$(".hamburger").click(function() { 
-    if($("#myNav").hasClass("active")) {
-        $("#myNav")
-            .css("height", "0%")
-            .removeClass("active");
-    }
+    // ========= Interactive Image using Interactive Image plugin =========
     
-  else {
-        $("#myNav")
-            .css("height", "100%")
-            .addClass("active");
-    }
-  
-});
+    // Chateau, Jean-Philippe. “jQuery interactive image plugin.”
+    //      https://github.com/jpchateau/Interactive-Image/
 
+    var options = {
+    shareBox:false
+    };
 
+    // Array for image hotspots
+    var notes = [
+    {
+      type: "text",
+      title: "Healthy Teeth",
+      description: "Integer rhoncus eros id nulla consectetur, at vulputate ligula vestibulum. Pellentesque suscipit pellentesque eleifend. Vivamus nec dignissim turpis, vulputate vestibulum lectus. Fusce eleifend fringilla sem.",
+      position: {
+        left: 80,
+        top: 100
+      },
+    },
+        
+    {
+      type: "text",
+      title: "Cognitive Ability",
+      description: "Integer rhoncus eros id nulla consectetur, at vulputate ligula vestibulum. Pellentesque suscipit pellentesque eleifend. Vivamus nec dignissim turpis, vulputate vestibulum lectus. Fusce eleifend fringilla sem.",
+      position: {
+        left: 100,
+        top: 10
+      },
+    },
+        
+    {
+      type: "text",
+      title: "Healthy Joints",
+      description: "Integer rhoncus eros id nulla consectetur, at vulputate ligula vestibulum. Pellentesque suscipit pellentesque eleifend. Vivamus nec dignissim turpis, vulputate vestibulum lectus. Fusce eleifend fringilla sem.",
+      position: {
+        left: 110,
+        top: 290
+      },
+    },
+     {
+      type: "text",
+      title: "Shiny Coat",
+      description: "Integer rhoncus eros id nulla consectetur, at vulputate ligula vestibulum. Pellentesque suscipit pellentesque eleifend. Vivamus nec dignissim turpis, vulputate vestibulum lectus. Fusce eleifend fringilla sem.",
+      position: {
+        left: 250,
+        top: 270
+      },
+    },
+    ];
+    
+    // Initalize the plugin passing in variables we set.
+    $('.interactive-image').interactiveImage(notes, options);
+
+    
+    
+    // ========= About Page image slider =========
+    
+    
+    $(function () {
+       var slider = $("#slides");
+
+        // Button click next 
+        slider.parent().on("click", "a#btn-prev", function(e){
+            e.preventDefault();
+            
+        var itemWidth = slider.parent().outerWidth();      
+            
+        slider.children("li:last").prependTo( slider );
+            
+        slider.css("left", -itemWidth);
+        slider.animate({left: 0}, 400, "swing");
+        });
+        
+        // Button click back
+        slider.parent().on("click", "a#btn-next", function(e){
+            e.preventDefault();
+        var itemWidth = slider.parent().outerWidth();  
+            
+        slider.animate({left: -itemWidth}, 400, "swing", function(){
+            slider.children("li:first").appendTo( slider );
+            slider.css("left", 0);
+            });
+        });               
+    });
+    
     
 });
